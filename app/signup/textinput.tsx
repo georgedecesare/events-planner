80dd6c0437cd4f1394de236f3e6c62d66c199256
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-06-26 20:28:50 +0100

    format

--- app/signup/textinput.tsx
+++ app/signup/textinput.tsx
@@ -15,7 +15,8 @@ export default function TextInput({
         type={type}
         placeholder={placeholder}
         onChange={(e) => onChangeAction(e.target.value)}
-        className="rounded-lg border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+        className="rounded-lg border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-blue-500
+          focus:outline-none"
       />
     </div>
   );
