9a30f7cb6194a870204abeb40736ad8ae20a45b6
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-06-27 21:07:12 +0100

    fix: Scrolling on weekdays

--- app/invitations/query2.tsx
+++ app/invitations/query2.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 
 export default function Query2Page() {
   return (
-    <div>
+    <>
       <h2 className="text-lg font-extrabold">Almost there!</h2>
       <p className="text-sm font-light">
         Please select the days of the week you&apos;ll prefer to attend an
         event.
       </p>
       <br />
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 overflow-scroll">
         <Day name="Monday" />
         <Day name="Tuesday" />
         <Day name="Wednesday" />
@@ -20,7 +20,7 @@ export default function Query2Page() {
         <Day name="Saturday" />
         <Day name="Sunday" />
       </div>
-    </div>
+    </>
   );
 }
 
