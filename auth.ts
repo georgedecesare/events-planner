ea2d9a51c8cccae1939f3559bd703923373e45a9
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-06-28 13:42:23 +0100

    feat: Add google as an oauth provider

--- auth.ts
+++ auth.ts
@@ -1,8 +1,14 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { Prisma } from '@prisma/client';
+import Google from 'next-auth/providers/google';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(Prisma),
-  providers: [],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID || '',
+      clientSecret: process.env.AUTH_GOOGLE_SECRET || '',
+    }),
+  ],
 });
