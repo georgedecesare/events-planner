51eea87a2f54db831e0ec4037585c3d71865725d
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-06-29 17:04:03 +0100

    fix

--- app/api/auth/[...next-auth]/route.ts
+++ app/api/auth/[...next-auth]/route.ts
@@ -1,22 +1,17 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/app/prisma';
 import GoogleProvider from 'next-auth/providers/google';
 
-// It's best practice to instantiate Prisma Client outside the handler
-// to avoid creating too many connections in a serverless environment.
-const prisma = new PrismaClient();
-
 const { handlers } = NextAuth({
-  // Here is the crucial part
   adapter: PrismaAdapter(prisma),
-
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
   ],
+  secret: process.env.AUTH_SECRET,
 });
 
 // Export HTTP method handlers for Next.js API routes
