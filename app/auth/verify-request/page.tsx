ea5d9928ac86435b66341cc6401befd6a1ef1287
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-07-01 18:01:47 +0100

    fix: Better colour

--- app/auth/verify-request/page.tsx
+++ app/auth/verify-request/page.tsx
@@ -2,7 +2,9 @@ export default function VerifyRequestPage() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="text-center m-5 py-20 w-full bg-gray-container p-10 rounded-2xl md:w-1/3">
-        <h1 className="text-2xl font-bold mb-4">Check your email</h1>
+        <h1 className="text-2xl font-bold mb-4 text-orange-accent">
+          Check your email
+        </h1>
         <p>
           We have sent you an email with a link to verify your account. Please
           check your inbox and click the link to continue.
